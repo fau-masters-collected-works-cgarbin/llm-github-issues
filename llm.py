2d0d01ebb3066f64e8bc17b1d2e1d4bad6214144
Christian Garbin <garbin.christian@gmail.com>
2023-10-21 17:22:25 -0400

    Fix usage of chat completion function

--- llm.py
+++ llm.py
@@ -13,20 +13,27 @@ MODEL = "gpt-3.5-turbo"
 
 SYSTEM_PROMPT = "You are an experience developer familiar with GitHub issues." + \
                 "The following text has been parsed from a GitHub issue." + \
-                "Answer the user request using the information provided."
+                "Answer the request using the information provided.\n" + \
+                "Extract the following information from the issue and comments:\n" +\
+                " - A brief summary of the issue in exactly one short sentence of no more 50 words.\n" + \
+                " - A more extended summary of the issue." + \
+                "   If code has been provided, list to the relevant pieces of code in the summary.\n" + \
+                " - A summary of each comment in chronological order as a markdown table with the columns date," + \
+                "   author, summary"
+
 
 CLIENT = None
 
 
 @dataclass
-class LLMAnswer:
-    """Class to hold the LLM answer.
+class LLMResponse:
+    """Class to hold the LLM response.
 
-    We use a class instead of returning the native LLM answer to make it easier to adapt to different LLMs later.
+    We use a class instead of returning the native LLM response to make it easier to adapt to different LLMs later.
     """
     system_prompt: Optional[str] = None
     user_input: Optional[str] = None
-    llm_answer: Optional[str] = None
+    llm_response: Optional[str] = None
     input_tokens: Optional[int] = None
     output_tokens: Optional[int] = None
 
@@ -46,25 +53,25 @@ def initialize() -> None:
     CLIENT = OpenAI(api_key=api_key)
 
 
-def _openai_chat_completion(system_prompt: str, user_input: str) -> LLMAnswer:
+def _openai_chat_completion(system_prompt: str, user_input: str) -> LLMResponse:
     """Get a completion from OpenAI."""
     completion = CLIENT.chat.completions.create(
-        model="gpt-4",
+        model=MODEL,
         messages=[
-            {"prompt": system_prompt, "text": input},
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_input},
         ],
         temperature=0.0  # We want precise and repeatable results
     )
-    answer = LLMAnswer()
+    answer = LLMResponse()
     answer.system_prompt = system_prompt
     answer.user_input = user_input
-    answer.llm_answer = completion.choices[0].text
+    answer.llm_response = completion.choices[0].message.content
 
     return answer
 
 
-def chat_completion(system_prompt: str, user_input: str) -> LLMAnswer:
+def chat_completion(system_prompt: str, user_input: str) -> LLMResponse:
     """Get a completion from the LLM."""
     # Only one LLM is currently supported. This function can be extended to support multiple LLMs later.
     return _openai_chat_completion(system_prompt, user_input)
