7133f43cb70307c5c23441a2c31e22819059c6fb
Christian Garbin <garbin.christian@gmail.com>
2023-10-28 17:44:46 -0400

    Record number of tokens and costs

--- llm.py
+++ llm.py
@@ -22,7 +22,13 @@ class LLMResponse:
     llm_response: Optional[str] = None
     input_tokens: Optional[int] = None
     output_tokens: Optional[int] = None
+    cost: Optional[float] = None
     raw_response: Optional[dict] = None
+
+    @property
+    def total_tokens(self):
+        """Calculate the total number of tokens used."""
+        return self.input_tokens + self.output_tokens
 
 
 def _get_openai_client() -> None:
@@ -34,6 +40,27 @@ def _get_openai_client() -> None:
         raise EnvironmentError("OPENAI_API_KEY environment variable not set -- see README.md for instructions")
 
     return OpenAI(api_key=api_key)
+
+
+def _openai_cost(input_tokens: int, output_tokens: int, model: str) -> float:
+    """Calculate the cost of the completion.
+
+    IMPORTANT: OpenAI mau change pricing at any time. Consult https://openai.com/pricing and
+    update this function accordingly.
+    """
+    # Price per 1,000 token for each model (from https://openai.com/pricing)
+    token_costs = {
+        "gpt-3.5-turbo": {"input": 0.0015, "completion": 0.002},
+        "gpt-3.5-turbo-16k": {"input": 0.003, "completion": 0.004},
+        "gpt-4": {"input": 0.03, "completion": 0.06},
+        "gpt-4-32k": {"input": 0.06, "completion": 0.12},
+    }
+
+    # Note that we use the model name without checking
+    # This is intentional to clearly flag when we need to update the code
+    input_cost = input_tokens * token_costs[model]["input"]
+    output_cost = output_tokens * token_costs[model]["completion"]
+    return input_cost + output_cost
 
 
 def _openai_chat_completion(model: str, prompt: str, user_input: str) -> LLMResponse:
@@ -49,12 +76,21 @@ def _openai_chat_completion(model: str, prompt: str, user_input: str) -> LLMResp
         ],
         temperature=0.0  # We want precise and repeatable results
     )
+    # Record the request and the response
     response = LLMResponse()
     response.model = model
     response.prompt = prompt
     response.user_input = user_input
     response.llm_response = completion.choices[0].message.content
     response.raw_response = completion
+    print(completion)
+
+    # Record the number of tokens used for input and output
+    response.input_tokens = completion.usage.prompt_tokens
+    response.output_tokens = completion.usage.completion_tokens
+
+    # Records costs (depends on the tokens and model - set them first)
+    response.cost = _openai_cost(response.input_tokens, response.output_tokens, model)
 
     return response
 
